10e870d6ebd0d3f1fb5df3b258616a6358c237fa
artem0190 <polimon0190123456789@gmail.com>
2023-01-18 22:25:55 +0500

    first commit

---
+++ src/containers/Header/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './header.css'
+
+const Header = () => {
+  return (
+    <div className="nav">
+      <div className="logo">Arem</div>
+      <div className="links">
+        <a href="#">Home</a>
+        <a href="#">Shoping</a>
+        <a href="#">seals</a>
+        <a href="#">Suport</a>
+      </div>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/containers/Main/Main.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import './main.css'
+
+const Main = () => {
+  return (
+    <div className="container">
+      <div className="foto">
+        <div className="foto1"></div>
+        <div className="text1">
+          <h1 className='e1'>Escape the city</h1>
+          <h1 className='welcome1'>Welcome to the quiet  eco house. Enjoy and get inspired.</h1>
+          <p className='our'>Our spacious and comfortable home is perfect for large groups
+               ,  and we have plenty of outdoor space for kids to run and play.
+               We  also have a private pool and hot tub  perfect for 
+               relaxing after a  long day of hiking or exploring the local 
+               </p>
+               <a href="#"className='readmore'>Read More</a>
+        </div>
+      </div>
+
+      <div className="foto3">
+        <div className="text2">
+          <h1 className='e2'>Escape the city</h1>
+          <h1 className='welcome2'>Welcome to the quiet  eco house. Enjoy and get inspired.</h1>
+          <p className='our2'>Our spacious and comfortable home is perfect for large groups
+               ,  and we have plenty of outdoor space for kids to run and play.
+               We  also have a private pool and hot tub  perfect for 
+               relaxing after a  long day of hiking or exploring the local 
+               </p>
+               <a href="#"className='readmore2'>Read More</a>
+        </div>        
+        <div className="foto2"></div>
+      </div>
+
+      <div className="foto">
+        <div className="foto1"></div>
+        <div className="text1">
+          <h1 className='e1'>Escape the city</h1>
+          <h1 className='welcome1'>Welcome to the quiet  eco house. Enjoy and get inspired.</h1>
+          <p className='our'>Our spacious and comfortable home is perfect for large groups
+               ,  and we have plenty of outdoor space for kids to run and play.
+               We  also have a private pool and hot tub  perfect for 
+               relaxing after a  long day of hiking or exploring the local 
+               </p>
+               <a href="#"className='readmore'>Read More</a>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Main
